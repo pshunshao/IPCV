4fb41a503d7783a6d8d28068f9e0f6fd97d295a4
Sonny Peng <sp15240@it025981.users.bris.ac.uk>
2017-12-01 18:41:48 +0000

    Setting up accumulator for the hough space, the original image can be transformed into --> grey --> grey blur --> edge images(Canny)

--- HoughTransform_Main.cpp
+++ HoughTransform_Main.cpp
@@ -9,16 +9,48 @@
 #include <iostream>
 #include <stdio.h>
 
+
 using namespace cv;
+
+void accumulator(int height, int width);
+
 int main() {
+  int img_height;
+  int img_width;
+
   Mat original_image;
-  original_image = imread("../Dart/dart0.jpg", 1);
+  original_image = imread("dart5.jpg", 1);
+
+  img_height = original_image.rows;
+  img_width = original_image.cols;
 
   Mat image_grey;
   cvtColor(original_image, image_grey, CV_BGR2GRAY);
 
-//  Mat image_grey_blur;
-//  blur(image_grey, image_grey_blur, Size(5,5));
+  Mat image_grey_blur;
+  blur(image_grey, image_grey_blur, Size(3,3));
 
+  Mat image_edge;
+  Canny(image_grey_blur, image_edge, 80, 150, 3);
+
+  accumulator(img_height, img_width);
+
+  imshow("Display", image_edge);
+
+  waitKey(0);
   return 0;
+}
+
+void accumulator(int height, int width){
+  double accum_height;
+  double accum_width;
+  double diagonal_distance;
+
+  diagonal_distance = sqrt((height * height) + (width * width));
+  accum_height = diagonal_distance * 2;
+  accum_width = 180;
+
+  cv::Mat accum(180, accum_height, CV_64FC1);
+
+
 }
